e3e4342146542b9d6a18d99038c36aff69da8f7c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-09 22:38:51 +0000

    Deploy kevinleung987/CSC318-ClassMood to github.com/kevinleung987/CSC318-ClassMood.git:gh-pages

---
+++ precache-manifest.f6a406cf5d68f41976b5273486b310b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bde10ee7cce3b523149e",
+    "url": "/static/css/main.b0168a2d.chunk.css"
+  },
+  {
+    "revision": "bde10ee7cce3b523149e",
+    "url": "/static/js/main.d0d3756d.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "7c2bfd91f6191e37c128",
+    "url": "/static/js/2.789776dd.chunk.js"
+  },
+  {
+    "revision": "93e0f1ef960f0fe3ec75a663f5f0bf6e",
+    "url": "/index.html"
+  }
+];
