67be473bc16b0afe4d675fd2776e96298068bcd3
Lysette <isjochan@gmail.com>
2019-03-09 21:35:31 -0500

    added tudent mood stat counts to lecture page

--- src/LecturerHome.js
+++ src/LecturerHome.js
@@ -23,7 +23,24 @@ class LecturerHome extends React.Component {
         </div></nav>
         <div className="container">
           <h3 className="center-align">Lecturer</h3>
-          <h5 className="center-align">Code: demo</h5>
+          <div className="row">
+              <div className="center-align col s12 m6 l3">
+                <h5 className="">Confused</h5>
+                <a className="">3</a>
+              </div>
+              <div className="center-align col s12 m6 l3">
+                <h5 className="">Question</h5>
+                <a className="">2</a>
+              </div>
+              <div className="center-align col s12 m6 l3">
+                <h5 className="">Hear</h5>
+                <a className="">5</a>
+              </div>
+              <div className="center-align col s12 m6 l3">
+                <h5 className="">Break</h5>
+                <a className="">8</a>
+              </div>
+          </div>         
           <QuestionsList questions={this.questions} />
         </div>
       </div>
