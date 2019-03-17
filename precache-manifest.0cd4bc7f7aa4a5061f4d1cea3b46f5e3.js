b41d322bce92079cd037b048e43ed9bed46f88b8
Kevin Leung <kevin.leung987@hotmail.com>
2019-03-17 17:06:17 -0400

    Updates

--- precache-manifest.0cd4bc7f7aa4a5061f4d1cea3b46f5e3.js
+++ precache-manifest.0cd4bc7f7aa4a5061f4d1cea3b46f5e3.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/csc318-classmood/static/js/runtime~main.a7c20bf1.js"
   },
   {
-    "revision": "405ba83de61a4a997ab8",
-    "url": "/csc318-classmood/static/js/main.071a3153.chunk.js"
+    "revision": "d37aa3b012ff905c5a11",
+    "url": "/csc318-classmood/static/js/main.1696c875.chunk.js"
   },
   {
     "revision": "f845ce85f9c4d9611c71",
     "url": "/csc318-classmood/static/js/2.f0f30078.chunk.js"
   },
   {
-    "revision": "405ba83de61a4a997ab8",
-    "url": "/csc318-classmood/static/css/main.a351b1f4.chunk.css"
+    "revision": "d37aa3b012ff905c5a11",
+    "url": "/csc318-classmood/static/css/main.83395679.chunk.css"
   },
   {
-    "revision": "1ffee4fc62286e7aba4eea3b29cf089e",
+    "revision": "ceb62bc63479815cb1651a452db78f69",
     "url": "/csc318-classmood/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/csc318-classmood/precache-manifest.8bcc54b87709d8a2eeb62a1d7b9c421d.js"
+  "/csc318-classmood/precache-manifest.0cd4bc7f7aa4a5061f4d1cea3b46f5e3.js"
 );
 
 workbox.clientsClaim();
