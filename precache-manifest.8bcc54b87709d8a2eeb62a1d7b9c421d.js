fa4aadc2e84278fb446729f16120b735a86bf862
Kevin Leung <kevin.leung987@hotmail.com>
2019-03-17 16:25:07 -0400

    Updates

--- precache-manifest.8bcc54b87709d8a2eeb62a1d7b9c421d.js
+++ precache-manifest.8bcc54b87709d8a2eeb62a1d7b9c421d.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/csc318-classmood/static/js/runtime~main.a7c20bf1.js"
   },
   {
-    "revision": "5b104d8ab661c90b75ba",
-    "url": "/csc318-classmood/static/js/main.ebee15a5.chunk.js"
+    "revision": "405ba83de61a4a997ab8",
+    "url": "/csc318-classmood/static/js/main.071a3153.chunk.js"
   },
   {
     "revision": "f845ce85f9c4d9611c71",
     "url": "/csc318-classmood/static/js/2.f0f30078.chunk.js"
   },
   {
-    "revision": "5b104d8ab661c90b75ba",
-    "url": "/csc318-classmood/static/css/main.9668aa74.chunk.css"
+    "revision": "405ba83de61a4a997ab8",
+    "url": "/csc318-classmood/static/css/main.a351b1f4.chunk.css"
   },
   {
-    "revision": "d46df93323c118580a636d86b2ca426c",
+    "revision": "1ffee4fc62286e7aba4eea3b29cf089e",
     "url": "/csc318-classmood/index.html"
   }
 ];
