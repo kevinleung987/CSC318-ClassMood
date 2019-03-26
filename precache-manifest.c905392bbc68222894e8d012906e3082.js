179e561b99eab004582ebe3fef066c13775fa22c
Kevin Leung <kevin.leung987@hotmail.com>
2019-03-25 22:44:37 -0400

    Updates

---
+++ precache-manifest.c905392bbc68222894e8d012906e3082.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a652847f3de9c53d32f5",
+    "url": "/csc318-classmood/static/js/runtime~main.a7c20bf1.js"
+  },
+  {
+    "revision": "13c0033b1ac22ecfbd5d",
+    "url": "/csc318-classmood/static/js/main.afa43bf6.chunk.js"
+  },
+  {
+    "revision": "9aa15384ab5d8bf9254e",
+    "url": "/csc318-classmood/static/js/2.82b223b4.chunk.js"
+  },
+  {
+    "revision": "13c0033b1ac22ecfbd5d",
+    "url": "/csc318-classmood/static/css/main.30996258.chunk.css"
+  },
+  {
+    "revision": "b9fa39719a626b822464e13c8bbd4bae",
+    "url": "/csc318-classmood/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/csc318-classmood/precache-manifest.4a524737930228524844e4d4ad776fcb.js"
+  "/csc318-classmood/precache-manifest.c905392bbc68222894e8d012906e3082.js"
 );
 
 workbox.clientsClaim();
